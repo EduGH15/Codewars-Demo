18a99f5c8e7e58a4096ee1a24b9262dd57d4c0c8
EduGH15 <eduardogl15.personal@gmail.com>
2022-08-03 10:45:03 -0300

    Se agregó un nuevo ejercicio sobre booleanos

---
+++ 8 kyu/yesOrNo.js
@@ -0,0 +1,22 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//My solution:
+
+function boolToWord( bool ){
+    return bool === true ? "Yes" : "No";
+  }
+
+
+// Other solutions:
+
+function boolToWord( bool ){
+    return bool ? 'Yes':'No';
+  }
+
+function boolToWord( bool ){
+    if (bool) {
+      return 'Yes';
+    } else {
+      return 'No';
+    }
+}
